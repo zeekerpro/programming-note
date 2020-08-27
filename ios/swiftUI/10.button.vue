a671ae21cf560fe0236263f3652bf71020dc83b7
zeeker <zeekerpro@gmail.com>
2020-08-27 17:59:44 +0800

    add swift/safeArea.md

---
+++ ios/swiftUI/10.button.vue
@@ -0,0 +1,15 @@
+button with action and element
+
+```
+Button(
+	action: {
+		self.showProfile.toggle()
+	}
+) {
+	Image("Avatar")
+		.renderingMode(.original)
+		.resizable()
+		.frame(width: 36, height: 36)
+		.clipShape(Circle())
+}
+```
